63e3842578ced1dbab3eb7e07ba677bf059eeffe
avamdui <54531433+avamdui@users.noreply.github.com>
2022-05-09 16:00:32 +0200

    Ajout CacheInterface

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -10,9 +10,12 @@ use Knp\Component\Pager\PaginatorInterface;
 use JMS\Serializer\Annotation\Groups;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class PhoneController extends AbstractController
 {
@@ -20,12 +23,16 @@ class PhoneController extends AbstractController
      * @route("/api/phones", name="api_phones", methods={"GET"})
      * @Groups({"phone:readall"})
      */
-    public function list(PhoneRepository $phoneRepository, SerializerInterface $serializer, Request $request, PaginatorInterface $paginator): JsonResponse
+    public function list(PhoneRepository $phoneRepository, SerializerInterface $serializer, Request $request, PaginatorInterface $paginator, CacheInterface $cache): Response
     {
         $phones = $phoneRepository->findAll();
         $phonesPaginate = $paginator->paginate($phones, $request->get('page', 1), 5);
         $data = $serializer->serialize($phonesPaginate, 'json', ['groups' => 'phone:readall']);
-        return new JsonResponse($data, JsonResponse::HTTP_OK, [], true);
+        $result = $cache->get('resultat', function (ItemInterface $item) use ($data, $phones) {
+            $item->expiresAfter(5);
+            return new Response($data, 200, array('Content-Type' => 'application/json'), $phones);
+        });
+        return $result;
     }
 
     /**
